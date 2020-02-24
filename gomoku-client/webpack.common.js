ca2addca4a7e89033c2a7f1e8688e109840796a7
stanley <stanleyhuang83@gmail.com>
2020-02-23 16:25:12 -0800

    webpack prod and dev configs

--- gomoku-client/webpack.common.js
+++ gomoku-client/webpack.common.js
@@ -6,11 +6,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
   module: {
     rules: [{
         test: /\.(png|svg|jpg|gif)$/,
